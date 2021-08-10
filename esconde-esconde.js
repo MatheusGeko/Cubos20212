f502a1cf0c943b30bc42d640ee146bfaad6bdf13
MatheusGeko <matheus_chiad@hotmail.com>
2021-08-10 16:52:46 -0400

    nostalgia

---
+++ esconde-esconde.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+  if (numero <= 20){
+      console.log ("VOCE CONSEGUE, MANINHO");
+  }else {
+      i = 21;
+      while(i <= numero){
+          console.log (i);
+          i++
+      }
+  }
+  }
+  
